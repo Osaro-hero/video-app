77da929cdc271ea3ef303badfb6390c706c4914b
osaro-hero <osaroosagie61@yahoo.com>
2022-05-23 08:19:40 +0100

    initial commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+const SearchBar = ({ onFormSubmit }) => {
+  const [term, setTerm] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    onFormSubmit(term);
+    // TODO: make sure  we call a callback from the parent component
+  };
+
+  return (
+    <div className="search-bar ui segment">
+      <form className="ui form" onSubmit={onSubmit}>
+        <div className="field">
+          <label>Video Search</label>
+          <input
+            type="text"
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
